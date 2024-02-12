924e9c74dbc5b6525fae97373dcf55481fa7227d
Sheela Thapa <61579439+sheelathapa@users.noreply.github.com>
2024-02-12 01:01:55 -0900

    second commit

---
+++ javacore-project/src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello java world.");
+        for (int i = 1; i <= 5 ; ++i) {
+            System.out.println(i);
+
+        }
+    }
+}
